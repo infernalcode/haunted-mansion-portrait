273df9fc22ca3b7284cf80ce073fdae775748dcf
Ryan Creasey <rcreasey@blizzard.com>
2021-10-27 12:24:48 -0700

    linting pass

--- code.py
+++ code.py
@@ -5,10 +5,7 @@ import board
 import neopixel
 from random import randrange
 import time
-from adafruit_led_animation.animation.solid import Solid
 from adafruit_led_animation.animation.pulse import Pulse
-from adafruit_led_animation.animation.comet import Comet
-from adafruit_led_animation.animation.sparklepulse import SparklePulse
 
 # Neopixel setup
 NUM_PIXELS = 100  # Number of pixels used in project
@@ -24,9 +21,6 @@ COLOR = (255, 255, 255)
 
 #  NeoPixel animations
 pulse = Pulse(strip, speed=0.01, color=COLOR, period=0.015)
-solid = Solid(strip, color=COLOR)
-comet = Comet(strip, speed=0.01, color=COLOR, tail_length=2)
-sparkle_pulse = SparklePulse(strip, speed=0.001, period=10, color=COLOR)
 
 # Audio Setup
 AUDIO_LIBRARY = [
@@ -49,30 +43,16 @@ def playSound(filename, callback=lambda: {}):
         callback()
         pass
 
-def lightning():
-  # number of flashes per cycle
-  flashCount = randrange(15, 25)
-
-  # flash duration range - ms
-  flashDurationMin = 25
-  flashDurationMax = 150
-
-  # time to next flash range - ms
-  nextFlashDelayMin = 25
-  nextFlashDelayMax = 75
-
-  # randomize pause between strikes
-  pauseBottom = 5000
-  pauseTop = 10000
-  strikeDelay = randrange(pauseBottom, pauseTop)
+def thunder():
+  flash_count = randrange(15, 25)
+  flash_duration_min = 25
+  flash_duration_max = 150
 
   # flash led while playing
-
-  for flash in range(flashCount):
+  for flash in range(flash_count):
     pulse.animate()
-    duration = randrange(flashDurationMin, flashDurationMax) / 1000
+    duration = randrange(flash_duration_min, flash_duration_max)
     time.sleep(duration)
-
 
 def delay():
   strip.fill(0)
@@ -84,5 +64,5 @@ def delay():
 
 while True:
   sound = randrange(0, 2)
-  playSound(sound, callback=lightning)
+  playSound(sound, callback=thunder)
   delay()
